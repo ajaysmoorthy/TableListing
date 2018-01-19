2c3969fc0338bfd19d937d1e2d581cb0ae55dceb
Official iPhone5s <apple@arackal.in>
2018-01-19 13:13:11 +0530

    Initial Commit

---
+++ TableListing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableListing
+//
+//  Created by Ajay S Moorthy on 19/01/18.
+//  Copyright © 2018 myyshopp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
